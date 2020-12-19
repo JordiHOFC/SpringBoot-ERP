c3e072bd3878e3da94557c2819709f47a93bd454
JordiHOFC <jordyhenrique7@gmail.com>
2020-12-18 22:36:01 -0300

    Add files via upload
    first commit

---
+++ loja/src/test/java/com/erp/loja/LojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
